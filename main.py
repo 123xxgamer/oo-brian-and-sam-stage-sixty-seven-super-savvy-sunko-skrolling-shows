84c5b59f365595bc3c2edff92c00015b50322f36
Samuel Sunko <ssunko60@stuy.edu>
2026-05-22 14:14:25 -0400

    basic variables

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from vpython import *
+
+m = 1.0 # mass (kg)
+R = 1.0 # radius (m)
+v_init = 5.0 # initial velocity (m/s)
+mu_k = 0.2 # coefficient of kinetic friction
+I_factor = 0.5 # moment of inertia factor for a solid disk
+
+running = False
+t = 0.0
+dt = 0.01
