612e25266f7119488c936bef5fd45365ab79456a
Samuel Sunko <ssunko60@stuy.edu>
2026-05-27 18:56:17 -0400

    buttons and sliders

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ g = 9.81 # acceleration due to gravity (m/s^2)
 v = v_init
 omega = 0.0 # initial angular velocity (rad/s)
 
-running = True
+running = False
 t = 0.0
 dt = 0.01
 
@@ -21,6 +21,55 @@ scene.camera.pos = vector(5, 1, 15)
 
 ground=box(pos=vector(10,0,0), size=vector(30,0.1,10), color=color.gray(0.5))
 object = cylinder(pos=vector(0, R, 0), axis=vector(0, 0, 0.5), radius=R, texture=textures.wood)
+
+#UI callback funcs
+def toggle_play(b):
+    global running
+    running = not running
+    # update button text
+
+def reset_sim(b):
+    global v, omega, running, t
+    running = False
+    t = 0.0
+    # update button text, reset positions and velocities
+
+def set_mass(s):
+    global m
+    m = s.value
+    #update text
+
+def set_fric(s):
+    global mu_k
+    mu_k = s.value
+    #update text
+
+def set_shape(m_item):
+    global I_factor, I
+    val = m_item.selected
+    # set I_factor based on shape and update I
+
+
+#UI elements
+scene.append_to_caption("\nControls:\n")
+button_play = button(text="Play", bind=toggle_play)
+button_reset = button(text="Reset", bind=reset_sim)
+scene.append_to_caption("\n\n")
+
+menu_shape = menu(choices=['Solid Cylinder', 'Hollow Cylinder', 'Solid Sphere', 'Hollow Sphere'], bind=set_shape)
+scene.append_to_caption("\n\n")
+
+slider_mass = slider(min=0.1, max=10.0, value=m, bind=set_mass, length=200)
+text_mass = wtext(text=f"Mass: {m:1.1f} kg")
+scene.append_to_caption("\n\n")
+
+slider_fric = slider(min=0.01, max=0.8, value=mu_k, bind=set_fric, length=200)
+text_fric = wtext(text=f"Kinetic Friction: {mu_k:1.2f}")
+scene.append_to_caption("\n\n")
+
+
+
+
 
 while True:
     rate(100)
